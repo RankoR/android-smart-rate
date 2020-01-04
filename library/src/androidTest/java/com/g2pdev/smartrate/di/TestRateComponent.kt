3278fa41463358cc1740685f1290e0e606a45b2d
Artem Smirnov <a.smirnov@labster.pro>
2020-01-04 22:33:52 +0300

    Never ask interactor test

--- library/src/androidTest/java/com/g2pdev/smartrate/di/TestRateComponent.kt
+++ library/src/androidTest/java/com/g2pdev/smartrate/di/TestRateComponent.kt
@@ -1,9 +1,10 @@
 package com.g2pdev.smartrate.di
 
 import com.g2pdev.smartrate.CachesTest
+import com.g2pdev.smartrate.RateRepositoryTest
 import com.g2pdev.smartrate.interactor.IsRatedInteractorTest
 import com.g2pdev.smartrate.interactor.LastPromptInteractorTest
-import com.g2pdev.smartrate.RateRepositoryTest
+import com.g2pdev.smartrate.interactor.NeverAskInteractorTest
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,5 +20,6 @@ internal interface TestRateComponent {
     fun inject(rateRepositoryTest: RateRepositoryTest)
     fun inject(isRatedInteractorTest: IsRatedInteractorTest)
     fun inject(lastPromptInteractorTest: LastPromptInteractorTest)
+    fun inject(neverAskInteractorTest: NeverAskInteractorTest)
 
 }
