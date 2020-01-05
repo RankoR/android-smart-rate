1c2da6aeb4342a6c61facc6ab24b4c47211897c3
Artem Smirnov <a.smirnov@labster.pro>
2020-01-05 22:32:57 +0300

    Fix library version

---
+++ app/src/androidTest/java/com/g2pdev/smartrate/demo/WillNotShowTest.kt
@@ -0,0 +1,23 @@
+package com.g2pdev.smartrate.demo
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.g2pdev.smartrate.demo.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class WillNotShowTest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+
+
+    @Test
+    fun testDialogWillNotShow() {
+
+    }
+}
