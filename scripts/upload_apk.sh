bae70db1500ae577dd7e2eaa5dd6fb1e44eb8d6d
Artem Smirnov <a.smirnov@labster.pro>
2020-01-07 02:03:20 +0300

    Add package name to the fastlane script

--- scripts/upload_apk.sh
+++ scripts/upload_apk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 gem install fastlane
-fastlane supply --apk app/build/outputs/apk/release/app-release.apk --track alpha --json_key scripts/google-play-auth.json
+fastlane supply --package_name com.g2pdev.smartrate.demo --apk app/build/outputs/apk/release/app-release.apk --track alpha --json_key scripts/google-play-auth.json
