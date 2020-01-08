8b0e8bc0ef4d92745d082d0ce040e09d292ba5a8
Artem Smirnov <a.smirnov@labster.pro>
2020-01-08 16:45:17 +0300

    Docs WIP

--- app/src/main/java/com/g2pdev/smartrate/demo/interactor/session_count/SetSessionCountBetweenPrompts.kt
+++ app/src/main/java/com/g2pdev/smartrate/demo/interactor/session_count/SetSessionCountBetweenPrompts.kt
@@ -3,6 +3,9 @@ package com.g2pdev.smartrate.demo.interactor.session_count
 import com.g2pdev.smartrate.demo.repository.SettingsRepository
 import io.reactivex.Completable
 
+/**
+ * Sets count of sessions between prompts (when user clicks «maybe later»)
+ */
 interface SetSessionCountBetweenPrompts {
     fun exec(sessionCount: Int): Completable
 }
